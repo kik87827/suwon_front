e6b20b8d5970fc1fcaf02529332aad03b8079328
kik8727 <kik87827@gmail.com>
2023-11-04 17:52:20 +0900

    분량 작업중

--- src/eng/assets/js/ui.js
+++ src/eng/assets/js/ui.js
@@ -716,3 +716,41 @@ $(function() {
       });
     }
   }
+
+
+  function scrollDrag(target){
+    let targetDom = [...target];
+    let touchstart = "ontouchstart" in window;
+    targetDom.forEach((item)=>{
+      scrollAction(item);
+    });
+    function scrollAction(targetItem){
+      if(touchstart){return;}
+      const slider = document.querySelector(targetItem);
+      let isDown = false;
+      let startX;
+      let scrollLeft;
+  
+      slider.addEventListener('mousedown', (e) => {
+        isDown = true;
+        slider.classList.add('active');
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+      });
+      slider.addEventListener('mouseleave', () => {
+        isDown = false;
+        slider.classList.remove('active');
+      });
+      slider.addEventListener('mouseup', () => {
+        isDown = false;
+        slider.classList.remove('active');
+      });
+      slider.addEventListener('mousemove', (e) => {
+        if(!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 3; //scroll-fast
+        slider.scrollLeft = scrollLeft - walk;
+      });
+    }
+  }
